39c2a03c60fbd35a96b0c0835c27bffa4e056cc9
hassaneljebyly <hassaneljeby@gmail.com>
2023-05-30 20:44:36 +0100

    feature to close last opened panel

--- Accordion/script.js
+++ Accordion/script.js
@@ -2,18 +2,22 @@ const accordion_1_Btn = Array.from(document.getElementsByClassName('accordion-1-
 
 accordion_1_Btn.forEach((accButton) => {
   accButton.addEventListener('click', () => {
-    togglePanel(accButton);
+    togglePanel(accButton, true);
   });
   accButton.addEventListener('keyup', ({ key }) => {
-    key === 'Enter' || key === ' ' ? togglePanel(accButton) : '';
+    key === 'Enter' || key === ' ' ? togglePanel(accButton, true) : '';
   });
 });
 
-function togglePanel(accButton) {
+function togglePanel(accButton, closeCurrentOpen) {
+  let currentOpenPanel = document.getElementsByClassName('open')[0];
   const targetPanelId = accButton.getAttribute('aria-controls');
   const targetPanel = document.getElementById(targetPanelId);
   const isOpen = targetPanel.classList.contains('open');
   const panelContent = targetPanel.children[1];
+
+  // if true close last opened panel if one is open
+  currentOpenPanel && closeCurrentOpen ? currentOpenPanel.classList.remove('open') : '';
   targetPanel.classList.toggle('open');
   accButton.setAttribute('aria-expanded', !isOpen);
   panelContent.setAttribute('aria-hidden', isOpen);
