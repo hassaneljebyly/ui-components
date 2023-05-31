b5a211964becc92f1c7599116ec2d44a0d8c651b
hassaneljebyly <hassaneljeby@gmail.com>
2023-05-31 14:00:55 +0100

    added accordion-2

--- Accordion/script.js
+++ Accordion/script.js
@@ -1,6 +1,15 @@
 const accordion_1_Btn = Array.from(document.getElementsByClassName('accordion-1-btn'));
+const accordion_2_Btn = Array.from(document.getElementsByClassName('accordion-2-btn'));
 
 accordion_1_Btn.forEach((accButton) => {
+  accButton.addEventListener('click', () => {
+    togglePanel(accButton, false);
+  });
+  accButton.addEventListener('keyup', ({ key }) => {
+    key === 'Enter' || key === ' ' ? togglePanel(accButton, false) : '';
+  });
+});
+accordion_2_Btn.forEach((accButton) => {
   accButton.addEventListener('click', () => {
     togglePanel(accButton, true);
   });
@@ -15,7 +24,6 @@ function togglePanel(accButton, closeCurrentOpen) {
   const targetPanel = document.getElementById(targetPanelId);
   const isOpen = targetPanel.classList.contains('open');
   const panelContent = targetPanel.children[1];
-
   // if true close last opened panel if one is open
   currentOpenPanel && closeCurrentOpen ? currentOpenPanel.classList.remove('open') : '';
   targetPanel.classList.toggle('open');
